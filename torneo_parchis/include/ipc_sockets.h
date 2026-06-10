14be12c77b6a9576bde4f35ba7e29323c28fe468
AndGSC <andrey.solis.carvajal@est.una.ac.cr>
2026-06-10 16:30:30 -0600

    Refactor ipc_sockets.h to remove unnecessary comments

--- torneo_parchis/include/ipc_sockets.h
+++ torneo_parchis/include/ipc_sockets.h
@@ -6,39 +6,12 @@
 #define PUERTO_BASE 5000
 #define TAM_BUFFER_SOCKET 128
 
-/*
- * Inicializa la estructura de sockets del árbitro.
- */
 void inicializar_sockets_arbitro(void);
-
-/*
- * Inicializa la conexión de socket de un jugador.
- */
 void inicializar_socket_jugador(int id_jugador);
-
-/*
- * Envía el turno desde el árbitro hacia un jugador.
- */
 void enviar_turno_socket(int id_jugador);
-
-/*
- * El jugador espera recibir su turno.
- */
 void esperar_turno_socket(int id_jugador);
-
-/*
- * El jugador envía una respuesta al árbitro.
- */
 void enviar_respuesta_socket(int id_jugador, const char *mensaje);
-
-/*
- * El árbitro recibe la respuesta de un jugador.
- */
 void recibir_respuesta_socket(int id_jugador);
-
-/*
- * Cierra los sockets usados por el árbitro y los jugadores.
- */
 void cerrar_sockets(void);
 
 #endif
