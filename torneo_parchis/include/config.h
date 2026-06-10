1debc7b09430a8638ba906d5563aa88bdbb41bc7
AndGSC <andrey.solis.carvajal@est.una.ac.cr>
2026-06-10 16:28:45 -0600

    Clean up config.h by removing unnecessary comments

--- torneo_parchis/include/config.h
+++ torneo_parchis/include/config.h
@@ -3,41 +3,15 @@
 
 
 #define NUM_JUGADORES 4
-
-
 #define NUM_FICHAS 4
-
-
 #define TAM_TABLERO 40
-
-/*
- * Posiciones especiales.
- * BASE indica que la ficha todavía no ha entrado al tablero.
- * META indica que la ficha ya terminó su recorrido.
- */
 #define BASE -1
 #define META 100
-
-/*
- * Límite máximo de turnos para evitar ejecuciones infinitas
- * en caso de que ningún jugador gane.
- */
 #define MAX_TURNOS 1000
-
-/*
- * Capacidad de las zonas especiales controladas con semáforos binarios.
- *
- * Al usar valor 1, solo una ficha puede entrar a la zona protegida
- * en un momento determinado.
- */
 #define CAPACIDAD_META 1
 #define CAPACIDAD_PASILLO 1
-
-
 #define DADO_MIN 1
 #define DADO_MAX 6
-
-
 #define SIN_GANADOR -1
 #define JUEGO_ACTIVO 0
 #define JUEGO_TERMINADO 1
